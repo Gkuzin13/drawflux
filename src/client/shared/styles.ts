b850f1133e8e416ef30bde15da69f0aacbf547b3
GKuzin13 <yo.is.josh@gmail.com>
2023-03-08 19:31:02 +0200

    reset styles

---
+++ src/client/shared/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from './theme';
+
+export const Button = styled('button', {
+  borderRadius: '8px',
+  border: '1px solid transparent',
+  padding: '$2',
+});
--- src/client/shared/theme.ts
+++ src/client/shared/theme.ts
@@ -2,6 +2,24 @@ import { createStitches } from '@stitches/react';
 
 export const { styled, css } = createStitches({
   theme: {
-    colors: {},
+    colors: {
+      black: '#FFF',
+      gray300: '#E0E0E0',
+      gray400: '#BDBDBD',
+      blue900: '#0D47A1',
+    },
+    space: {
+      1: '4px',
+      2: '8px',
+      3: '12px',
+      4: '16px',
+      5: '20px',
+      6: '24px',
+    },
+    fontSizes: {
+      1: '12px',
+      2: '14px',
+      3: '16px',
+    },
   },
 });
