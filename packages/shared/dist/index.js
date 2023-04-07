20537a7da31a42c7fa87185ad9ae2280a96c8d1d
GKuzin13 <yo.is.josh@gmail.com>
2023-04-07 18:00:26 +0300

    ignore fixes

--- packages/shared/dist/index.js
+++
@@ -1,3 +0,0 @@
-export * as Schemas from './schemas/index';
-export * from './design/design';
-export * from './types/index';
--- packages/shared/dist/types/node.js
+++
@@ -1,2 +0,0 @@
-import { Node } from '../schemas/node';
-const { nodeProps, style, type } = Node.shape;
