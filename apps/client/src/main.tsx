317ae6bb88605cf10e359470c28105058188bc7e
GKuzin13 <yo.is.josh@gmail.com>
2023-04-13 12:30:06 +0300

    add redirect

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider as StoreProvider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 import Root from './routes/Root';
 import { store } from './stores/store';
 import { globalStyle } from './styles/global';
@@ -14,6 +18,10 @@ const router = createBrowserRouter([
   {
     path: '/p/:id',
     element: <Root />,
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" />,
   },
 ]);
 
