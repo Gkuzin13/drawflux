de348463d68deff2ab1a4b9bd66091b65e6eadbc
GKuzin13 <yo.is.josh@gmail.com>
2023-04-15 17:39:51 +0300

    add host port

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: { port: 5174 },
   plugins: [react()],
   resolve: {
     alias: {
