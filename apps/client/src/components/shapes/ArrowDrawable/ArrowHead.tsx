e24a0ee7477510bf834b8498d30df9f1790a7930
GKuzin13 <yo.is.josh@gmail.com>
2023-04-29 00:47:38 +0300

    refactor: create drawHead function

--- apps/client/src/components/shapes/ArrowDrawable/ArrowHead.tsx
+++ apps/client/src/components/shapes/ArrowDrawable/ArrowHead.tsx
@@ -1,5 +1,6 @@
+import type Konva from 'konva';
 import { type NodeConfig } from 'konva/lib/Node';
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
 import { Shape } from 'react-konva';
 import { type Point } from 'shared';
 
@@ -9,39 +10,36 @@ type Props = {
   config: NodeConfig;
 };
 
-const ArrowHead = memo(({ control, end, config }: Props) => {
-  return (
-    <Shape
-      {...config}
-      sceneFunc={(ctx, shape) => {
-        const PI2 = Math.PI * 2;
+const ArrowHead = ({ control, end, config }: Props) => {
+  const drawHead = useCallback(
+    (ctx: Konva.Context, shape: Konva.Shape) => {
+      const PI2 = Math.PI * 2;
+      const dx = end[0] - control[0];
+      const dy = end[1] - control[1];
 
-        const dx = end[0] - control[0];
-        const dy = end[1] - control[1];
+      const radians = (Math.atan2(dy, dx) + PI2) % PI2;
+      const length = 3 * config.strokeWidth;
+      const width = 4 * config.strokeWidth;
 
-        const radians = (Math.atan2(dy, dx) + PI2) % PI2;
-        const length = 3 * config.strokeWidth;
-        const width = 4 * config.strokeWidth;
+      ctx.save();
 
-        ctx.save();
+      ctx.beginPath();
+      ctx.translate(end[0], end[1]);
+      ctx.rotate(radians);
 
-        ctx.beginPath();
-        ctx.translate(end[0], end[1]);
-        ctx.rotate(radians);
+      ctx.moveTo(0, 0);
+      ctx.lineTo(-length, width / 2);
 
-        ctx.moveTo(0, 0);
-        ctx.lineTo(-length, width / 2);
+      ctx.moveTo(0, 0);
+      ctx.lineTo(-length, -width / 2);
 
-        ctx.moveTo(0, 0);
-        ctx.lineTo(-length, -width / 2);
-
-        ctx.restore();
-        ctx.fillStrokeShape(shape);
-      }}
-    />
+      ctx.restore();
+      ctx.fillStrokeShape(shape);
+    },
+    [config.strokeWidth, control, end],
   );
-});
 
-ArrowHead.displayName = 'ArrowHead';
+  return <Shape {...config} sceneFunc={drawHead} />;
+};
 
 export default ArrowHead;
