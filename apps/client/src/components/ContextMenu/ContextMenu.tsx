97ce89acf44bf08ff6d6640aa77802a8c4bc55b8
GKuzin13 <yo.is.josh@gmail.com>
2023-05-03 23:01:28 +0300

    refactor: change context menu rendering method

--- apps/client/src/components/ContextMenu/ContextMenu.tsx
+++ apps/client/src/components/ContextMenu/ContextMenu.tsx
@@ -1,32 +1,98 @@
-import type { ContextMenuItem } from '@/constants/menu';
+import type { Vector2d } from 'konva/lib/types';
+import { memo, useCallback, useMemo } from 'react';
+import { Html } from 'react-konva-utils';
+import { Provider as StoreProvider, useStore } from 'react-redux';
+import type { SelectedNodesIds } from '@/constants/app';
+import { useAppDispatch, useAppSelector } from '@/stores/hooks';
+import { canvasActions, selectCanvas } from '@/stores/slices/canvasSlice';
+import { contextMenuActions } from '@/stores/slices/contextMenu';
+import { nodesActions, selectNodes } from '@/stores/slices/nodesSlice';
+import { Divider } from '../core/Divider/Divider';
 import Menu from '../core/Menu/Menu';
+import { ContextMenuContainer } from './ContextMenuStyled';
+
+export type ContextMenuType = 'node-menu' | 'drawing-canvas-menu';
 
 type Props = {
-  items: ContextMenuItem[];
-  onAction: (key: ContextMenuItem['key']) => void;
+  type: ContextMenuType;
+  position: Vector2d;
+  opened: boolean;
 };
 
-const NodeMenu = ({ items, onAction }: Props) => {
+const DrawingCanvasMenu = () => {
+  const { nodes } = useAppSelector(selectNodes).present;
+
+  const dispatch = useAppDispatch();
+
+  const handleSelectAll = useCallback(() => {
+    const allSelectedNodesIds: SelectedNodesIds = {};
+
+    for (const node of nodes) {
+      allSelectedNodesIds[node.nodeProps.id] = true;
+    }
+
+    dispatch(canvasActions.setSelectedNodesIds(allSelectedNodesIds));
+    dispatch(contextMenuActions.close());
+  }, [dispatch, nodes]);
+
+  const handleSelectNone = useCallback(() => {
+    dispatch(canvasActions.setSelectedNodesIds({}));
+    dispatch(contextMenuActions.close());
+  }, [dispatch]);
+
   return (
-    <Menu.Dropdown opened={true}>
-      {items.map((item) => {
-        return (
-          <Menu.Item
-            key={item.key}
-            title={item.name}
-            fullWidth={true}
-            spanned={true}
-            size="small"
-            color="secondary-light"
-            onItemClick={() => onAction(item.key)}
-          >
-            {item.name}
-            {item.icon && item.icon({})}
-          </Menu.Item>
-        );
-      })}
-    </Menu.Dropdown>
+    <>
+      <Divider type="horizontal" />
+      <Menu.Item onItemClick={handleSelectAll}>Select All</Menu.Item>
+      <Menu.Item onItemClick={handleSelectNone}>Select None</Menu.Item>
+    </>
   );
 };
 
-export default NodeMenu;
+const NodeMenu = () => {
+  const { selectedNodesIds } = useAppSelector(selectCanvas);
+  const dispatch = useAppDispatch();
+
+  const handleDeleteNode = useCallback(() => {
+    dispatch(nodesActions.delete(Object.keys(selectedNodesIds)));
+    dispatch(contextMenuActions.close());
+  }, [dispatch, selectedNodesIds]);
+
+  return (
+    <>
+      <Menu.Item onItemClick={handleDeleteNode}>Delete</Menu.Item>
+    </>
+  );
+};
+
+const ContextMenu = memo(({ type, position, opened = false }: Props) => {
+  const store = useStore();
+
+  const ActiveMenu = useMemo(() => {
+    switch (type) {
+      case 'node-menu':
+        return NodeMenu;
+      case 'drawing-canvas-menu':
+        return DrawingCanvasMenu;
+    }
+  }, [type]);
+
+  return (
+    <Html
+      groupProps={{ ...position }}
+      transformFunc={(attrs) => {
+        return { ...attrs, scaleX: 1, scaleY: 1 };
+      }}
+    >
+      <StoreProvider store={store}>
+        <ContextMenuContainer opened={opened}>
+          <ActiveMenu />
+        </ContextMenuContainer>
+      </StoreProvider>
+    </Html>
+  );
+});
+
+ContextMenu.displayName = 'ContextMenu';
+
+export default ContextMenu;
--- apps/client/src/constants/menu.ts
+++ apps/client/src/constants/menu.ts
@@ -1,30 +1,6 @@
-import { IoTrashOutline } from 'react-icons/io5';
 import { TbFileDownload, TbFileUpload, TbPhotoDown } from 'react-icons/tb';
 
-export type ContextMenuItem =
-  | (typeof STAGE_CONTEXT_MENU_ACTIONS)[number]
-  | (typeof NODE_CONTEXT_MENU_ACTIONS)[number];
-
 export type MenuPanelActionType = (typeof MENU_PANEL_ACTIONS)[number]['key'];
-
-export const NODE_CONTEXT_MENU_ACTIONS = [
-  {
-    key: 'delete-node',
-    name: 'Delete',
-    icon: IoTrashOutline,
-  },
-] as const;
-
-export const STAGE_CONTEXT_MENU_ACTIONS = [
-  {
-    key: 'select-all',
-    name: 'Select All',
-  },
-  {
-    key: 'select-none',
-    name: 'Select None',
-  },
-] as const;
 
 export const MENU_PANEL_ACTIONS = [
   {
