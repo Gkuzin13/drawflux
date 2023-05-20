0c80b452f9e3c9014146327071e61f766184c786
GKuzin13 <yo.is.josh@gmail.com>
2023-05-20 13:57:19 +0300

    fix: blurry QRCode image

--- apps/client/src/components/QRCode/QRCodeStyled.ts
+++ apps/client/src/components/QRCode/QRCodeStyled.ts
@@ -1,8 +1,7 @@
 import { styled } from 'shared';
 
 export const QRCodeBgImage = styled('div', {
-  backgroundSize: '100% 100%',
-  backgroundRepeat: 'repeat',
+  backgroundPosition: 'center',
   width: '100%',
   height: '100%',
 });
