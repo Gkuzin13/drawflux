6746628af314b46de1c39ebd44c3049ac28c5694
GKuzin13 <yo.is.josh@gmail.com>
2023-06-01 11:13:22 +0300

    feat: add offline support with vite-plugin-pwa

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,11 +1,44 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: { port: 5174 },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: { enabled: true },
+      workbox: {
+        globPatterns: ['**/*'],
+      },
+      includeAssets: ['**/*'],
+      manifest: {
+        name: 'drawflux',
+        short_name: 'Drawflux',
+        display: 'standalone',
+        description: 'A web app to create and share drawings',
+        theme_color: '#43A047',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@/': new URL('./src/', import.meta.url).pathname,
