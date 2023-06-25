6faf5cf457017546b8eb37397597fad5e114a98c
GKuzin13 <yo.is.josh@gmail.com>
2023-06-25 21:29:54 +0300

    chore: add ws provider

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider as StoreProvider } from 'react-redux';
+import { WebSocketProvider } from '@/contexts/websocket';
 import App from './App';
 import { store } from './stores/store';
 import { globalStyle } from './styles/global';
-import { WebSocketProvider } from './webSocketContext';
 
 globalStyle();
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <StoreProvider store={store}>
-      <WebSocketProvider>
+    <WebSocketProvider>
+      <StoreProvider store={store}>
         <App />
-      </WebSocketProvider>
-    </StoreProvider>
+      </StoreProvider>
+    </WebSocketProvider>
   </React.StrictMode>,
 );
