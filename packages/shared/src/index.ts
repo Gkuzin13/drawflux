3fa596302c6d937651a05b50abdaac418ec07eed
GKuzin13 <yo.is.josh@gmail.com>
2023-06-28 11:09:57 +0300

    chore: add animations

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,7 @@
 export * from './schemas/index';
 export * from './design/design';
+export * from './design/animation';
+export * from './design/keyframes';
 export * from './types/index';
 export * from './utils/errors';
 export * from './utils/websocket';
