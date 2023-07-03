0dbb9491b1f147aabc94af56ea1992fbe1b71bee
GKuzin13 <yo.is.josh@gmail.com>
2023-07-03 12:15:37 +0300

    chore: add path alias

--- apps/server/vitest.config.ts
+++ apps/server/vitest.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname,
+    },
+  },
   test: {
     globals: true,
     environment: 'node',
