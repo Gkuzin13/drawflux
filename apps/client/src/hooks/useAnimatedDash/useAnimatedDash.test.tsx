9d21479f55cc30489e0142180322f7808b4388dd
GKuzin13 <yo.is.josh@gmail.com>
2023-07-13 10:50:38 +0300

    fix: incorrect animation timing when dash changes

--- apps/client/src/hooks/useAnimatedDash/useAnimatedDash.test.tsx
+++ apps/client/src/hooks/useAnimatedDash/useAnimatedDash.test.tsx
@@ -3,21 +3,42 @@ import { Line } from 'konva/lib/shapes/Line';
 import useAnimatedDash from './useAnimatedDash';
 
 describe('useAnimatedDash', () => {
-  it('should run animation', async () => {
-    const elementRef = {
-      current: new Line(),
-    };
+  const nodeRef = {
+    current: new Line(),
+  };
 
+  const initialProps = {
+    enabled: false,
+    nodeRef,
+    totalDashLength: 8,
+  };
+
+  it('toggles animation when enabled changes', async () => {
     const { result, rerender } = renderHook((args) => useAnimatedDash(args), {
-      initialProps: { enabled: false, elementRef, totalDashLength: 8 },
+      initialProps,
     });
 
     expect(result.current.animation).toBe(null);
 
-    rerender({ enabled: true, elementRef, totalDashLength: 8 });
+    rerender({ enabled: true, nodeRef, totalDashLength: 8 });
     expect(result.current.animation?.isRunning()).toBe(true);
 
-    rerender({ enabled: false, elementRef, totalDashLength: 8 });
-    expect(result.current.animation).toBe(null);
+    rerender({ enabled: false, nodeRef, totalDashLength: 8 });
+    expect(result.current.animation?.isRunning()).toBe(false);
+  });
+
+  it("does't create a new animation instance when totalDashLength changes", async () => {
+    const { result, rerender } = renderHook((args) => useAnimatedDash(args), {
+      initialProps,
+    });
+
+    rerender({ enabled: true, nodeRef, totalDashLength: 8 });
+    const initialAnimationId = result.current.animation?.id;
+
+    rerender({ enabled: true, nodeRef, totalDashLength: 12 });
+    expect(result.current.animation?.id).toBe(initialAnimationId);
+
+    rerender({ enabled: true, nodeRef, totalDashLength: 18 });
+    expect(result.current.animation?.id).toBe(initialAnimationId);
   });
 });
