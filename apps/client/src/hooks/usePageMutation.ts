ab238528038ce74eee9a4b1377fd65177e935dcc
GKuzin13 <yo.is.josh@gmail.com>
2023-08-02 11:00:58 +0300

    refactor: update fetch args

--- apps/client/src/hooks/usePageMutation.ts
+++ apps/client/src/hooks/usePageMutation.ts
@@ -1,18 +1,24 @@
 import type { UpdatePageRequestBody, UpdatePageResponse } from 'shared';
-import useFetch from './useFetch';
+import useFetch, { type UseFetchConfig } from './useFetch';
 import { useEffect } from 'react';
 import { useNotifications } from '@/contexts/notifications';
 import useUrlSearchParams from './useUrlSearchParams/useUrlSearchParams';
 import { PAGE_URL_SEARCH_PARAM_KEY } from '@/constants/app';
 
+const options: UseFetchConfig = {
+  method: 'PATCH',
+};
+
 function usePageMutation() {
   const params = useUrlSearchParams();
+
   const pageId = params[PAGE_URL_SEARCH_PARAM_KEY];
+  const url = `/p/${pageId}`;
 
   const [{ error }, updatePage] = useFetch<
     UpdatePageResponse,
     UpdatePageRequestBody
-  >(`/p/${pageId}`, { method: 'PATCH' }, { skip: true });
+  >(url, options, true);
 
   const { addNotification } = useNotifications();
 
