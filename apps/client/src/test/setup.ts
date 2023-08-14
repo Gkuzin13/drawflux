561976d84f3b8034c17eb3e66192378daf5e5cad
GKuzin13 <yo.is.josh@gmail.com>
2023-08-14 10:13:01 +0300

    chore: fix 'jest' undefined error on canvas tests

--- apps/client/src/test/setup.ts
+++ apps/client/src/test/setup.ts
@@ -1,13 +1,20 @@
 import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
-import { expect, afterEach } from 'vitest';
 import ResizeObserverPolyfill from 'resize-observer-polyfill';
 import 'vitest-canvas-mock';
 
-expect.extend(matchers);
+/**
+ * temporary fix for tests that use jest-canvas-mock
+ * throws this error: ReferenceError: jest is not defined
+ */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+globalThis.jest = vi;
 
 afterEach(() => {
   cleanup();
 });
+
+expect.extend(matchers);
 
 global.ResizeObserver = ResizeObserverPolyfill;
