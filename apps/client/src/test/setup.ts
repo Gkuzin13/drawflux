9c95aa5e274c6b4d64ff5f0c22732ac233a84c5d
gkuzin13 <gkuzin22@gmail.com>
2023-12-09 22:58:29 +0200

    chore: fixed typeError during test run

--- apps/client/src/test/setup.ts
+++ apps/client/src/test/setup.ts
@@ -1,4 +1,4 @@
-import matchers from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 import ResizeObserverPolyfill from 'resize-observer-polyfill';
 import 'vitest-canvas-mock';
