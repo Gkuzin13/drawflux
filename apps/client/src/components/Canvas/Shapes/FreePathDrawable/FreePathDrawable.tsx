0096c8ac819caed27f8223bf3b83a6202cabdf55
gkuzin13 <gkuzin22@gmail.com>
2023-12-09 23:01:50 +0200

    feat: added path curves

--- apps/client/src/components/Canvas/Shapes/FreePathDrawable/FreePathDrawable.tsx
+++ apps/client/src/components/Canvas/Shapes/FreePathDrawable/FreePathDrawable.tsx
@@ -119,6 +119,7 @@ const FreePathDrawable = ({
         ref={nodeRef}
         points={flattenedPoints}
         {...config}
+        bezier={true}
         onDragEnd={handleDragEnd}
         onTransformStart={handleTransformStart}
         onTransform={handlTransform}
